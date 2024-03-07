ca6a94aa721d7cdd6526a9942f9fa6a378b18085
kavwad <123405488+kavwad@users.noreply.github.com>
2024-03-07 09:20:52 -0800

    add data editor upload functionality

--- data_editor.py
+++ data_editor.py
@@ -1,10 +1,17 @@
+import time
 import streamlit as st
 
-from import_pdf import load_data
+from import_pdf import load_data, save_to_gcs
 
 rider = st.radio('Choose your rider',
                  ['B', 'K'],
                  horizontal=True,
                  label_visibility='hidden')
 df = load_data(rider)
-st.data_editor(df, hide_index=True, height=500)
+df_edited = st.data_editor(df, hide_index=True, height=500)
+
+if st.button('Save changes', type='primary'):
+    save_to_gcs(rider, df_edited)
+    st.success(f'Saved!', icon='🚍')
+    time.sleep(3)
+    st.rerun()
